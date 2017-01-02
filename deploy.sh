8ab21f51984c0ae3b5341e3a9a20d2d8e60c95ef
Sanjoy Das <sanjoy@playingwithpointers.com>
2017-01-01 17:11:53 -0800

    deploy: don't upload broken build

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-jekyll build
-$(dirname $0)/generate-meta.sh
+jekyll build && \
+$(dirname $0)/generate-meta.sh && \
 rsync --progress -av _site/ sanjoyd_playingwithpointers@ssh.phx.nearlyfreespeech.net:/home/public/
